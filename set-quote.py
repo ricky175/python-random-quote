d907fd21c815fc71a37c31b6607325b2814d80be
ricky175 <derRaxxer11@gmail.com>
2020-11-09 19:12:01 +0100

    quote adder added

--- set-quote.py
+++ set-quote.py
@@ -2,5 +2,6 @@ def main():
     with open("quotes.txt", "a") as file:
         file.writelines(["Never give up, never surrender\n", "I will protect you\n", "I believe in you\n"])
 
+
 if __name__ =="__main__":
     main()
